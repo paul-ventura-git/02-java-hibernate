8bacdb3f432448012d4cce5f2cb5b1891e8c629b
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-06 20:24:17 -0500

    Entities

---
+++ src/main/java/com/javacodegeeks/ultimate/entity/Auditable.java
@@ -0,0 +1,10 @@
+package com.javacodegeeks.ultimate.entity;
+
+import java.util.Date;
+
+public interface Auditable {
+	Date getCreated();
+	void setCreated(Date created);
+	Date getLastUpdate();
+	void setLastUpdate(Date lastUpdate);
+}
